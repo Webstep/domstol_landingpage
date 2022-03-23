47fa99239eccc95727414c57bc3e053a0f9ffb24
karenhompland <khompland@gmail.com>
2022-03-23 15:45:26 +0100

    feat(#6) added pixelated man component with animation

---
+++ components/common/PixelatedMan/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+const imgVariants = {
+    start: {
+        opacity: 0,
+        scale: 0.9 
+    },
+    end: {
+        opacity: 100, 
+        scale: 1
+    }
+}
+
+const PixelatedMan = () => {
+    return (
+        <>
+        <motion.img
+            variants={imgVariants}
+            initial="start"
+            animate="end"
+            transition={{ 
+                opacity: {duration: 18},
+                scale: {duration: 1}
+            }}
+            src="/pixelatedManWhite.svg"
+            alt="Figure" >
+        </motion.img>
+        </>
+    );
+};
+
+export default PixelatedMan;
