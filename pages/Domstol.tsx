a03fc6bde7d458f8e0c42687a9cdff3d8c4a3d35
André Schjøth <aschjoth@gmail.com>
2022-04-27 14:48:49 +0200

    feat(#31): create hairy goals component

--- pages/Domstol.tsx
+++ pages/Domstol.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ConvictedPercent from '../components/slides/domstol/ConvictedPercent'
 import ExampleSlide from '../components/slides/domstol/ExampleSlide'
 import AboutUs from '../components/slides/domstol/AboutUs'
+import HairyGoals from '../components/slides/domstol/HairyGoals'
 
 const Domstol = () => {
     return (
         <>
             <ConvictedPercent />
+            <HairyGoals />
             <AboutUs />
             <ExampleSlide />
         </>
