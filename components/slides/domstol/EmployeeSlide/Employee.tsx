0dba7f81fcfa913bba994ca42ab4a04933b2bceb
Emilie Dybendal Aulie <59264023+emilieaulie@users.noreply.github.com>
2022-04-27 15:31:14 +0200

    Added scroll-function of new employee

--- components/slides/domstol/EmployeeSlide/Employee.tsx
+++ components/slides/domstol/EmployeeSlide/Employee.tsx
@@ -1,74 +1,120 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Employee.module.scss'
 import Image from 'next/image'
-import {
-    AnimatePresence,
-    motion,
-    useElementScroll,
-    useViewportScroll,
-} from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import Link from 'next/link'
+import EmployeeCalle from './employeeCalle'
+import EmployeeBirgitte from './employeeBirgitte'
 
 const Employee = () => {
     //const { scrollYProgress } = useViewportScroll()
+    const [lastYPos, setLastYPos] = useState(0)
+    const [showNewEmployee, setShowNewEmployee] = useState(false)
+
+    useEffect(() => {
+        function handleScroll() {
+            const yPos = window.scrollY
+            const isScrollingDown = yPos > lastYPos
+            console.log('scrolled')
+            console.log(yPos)
+
+            setShowNewEmployee(isScrollingDown)
+            setLastYPos(yPos)
+        }
+        window.addEventListener('scroll', handleScroll, false)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll, false)
+        }
+    }, [lastYPos])
     return (
         <section className={styles.section}>
             <AnimatePresence>
-                <motion.div
-                //style={{ scaleX: scrollYProgress }}
-                >
+                <motion.div>
                     <div className={styles.container}>
                         <motion.div animate={{ position: 'sticky' }}>
                             <h1>MØT VÅRE WEBSTEPPERE</h1>
                         </motion.div>
-                        <div className={styles.content}>
-                            <div>
-                                <motion.div
-                                    className={styles.images}
-                                    initial={{ y: 200 }}
-                                    whileInView={{ y: 0 }}
-                                    transition={{ duration: 1 }}
-                                >
-                                    <Image
-                                        src={'/assets/employeeBirgitte.svg'}
-                                        layout="fixed"
-                                        width="250vw"
-                                        height="350vw"
-                                        alt={'employeeBirgitte'}
-                                    />
-                                </motion.div>
-                                <motion.div
-                                    className={styles.moreInfo}
-                                    initial={{ y: 125 }}
-                                    whileInView={{ y: 0 }}
-                                    transition={{ duration: 2 }}
-                                >
-                                    Les mer om Birgitte
-                                    <Link href={'#'}>her</Link>
-                                </motion.div>
-                            </div>
-                            <motion.div
-                                className={styles.textSection}
-                                initial={{ y: 200 }}
-                                whileInView={{ y: 0 }}
-                                transition={{ duration: 1 }}
-                            >
-                                <p className={styles.quote}>
-                                    - Noe av det beste med å være en del av
-                                    dette prosjektet, er alt det nye jeg har
-                                    lært, innsikten i hvordan rettsvesenet i
-                                    Norge fungerer.
-                                </p>
-                                <p className={styles.name}>Birgitte Bright</p>
-                                <p className={styles.title}>
-                                    Systemutvikler, Webstep
-                                </p>
-                            </motion.div>
-                        </div>
+                        {showNewEmployee ? (
+                            <EmployeeCalle />
+                        ) : (
+                            <EmployeeBirgitte />
+                        )}
                     </div>
                 </motion.div>
             </AnimatePresence>
         </section>
+    )
+}
+
+function Actions() {
+    const [lastYPos, setLastYPos] = useState(0)
+    const [showNewEmployee, setShowNewEmployee] = useState(false)
+
+    useEffect(() => {
+        function handleScroll() {
+            const yPos = window.scrollY
+            const isScrollingDown = yPos > lastYPos
+            console.log('scrolled')
+
+            setShowNewEmployee(isScrollingDown)
+            setLastYPos(yPos)
+        }
+        window.addEventListener('scroll', handleScroll, false)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll, false)
+        }
+    }, [lastYPos])
+    return (
+        <motion.div
+            className="actions"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: showNewEmployee ? 1 : 0 }}
+            transition={{ opacity: { duration: 0.2 } }}
+        >
+            <div className={styles.content}>
+                <div>
+                    <motion.div
+                        className={styles.images}
+                        initial={{ y: 200 }}
+                        whileInView={{ y: 0 }}
+                        transition={{ duration: 1 }}
+                    >
+                        <Image
+                            src={'/assets/employeeCalle.svg'}
+                            layout="fixed"
+                            width="250vw"
+                            height="350vw"
+                            alt={'employeeCalle'}
+                        />
+                    </motion.div>
+                    <motion.div
+                        className={styles.moreInfo}
+                        initial={{ y: 125 }}
+                        whileInView={{ y: 0 }}
+                        transition={{ duration: 2 }}
+                    >
+                        Les mer om Calle
+                        <Link href={'#'}>her</Link>
+                    </motion.div>
+                </div>
+                <motion.div
+                    className={styles.textSection}
+                    initial={{ y: 200 }}
+                    whileInView={{ y: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                    <p className={styles.quote}>
+                        - Noe av det beste med å være en del av dette
+                        prosjektet, er alt det nye jeg har lært, innsikten i
+                        hvordan rettsvesenet i Norge fungerer.
+                    </p>
+                    <p className={styles.name}>Birgitte Bright</p>
+                    <p className={styles.title}>Systemutvikler, Webstep</p>
+                </motion.div>
+            </div>
+        </motion.div>
     )
 }
 
---
+++ components/slides/domstol/EmployeeSlide/employeeCalle.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import styles from './Employee.module.scss'
+import Image from 'next/image'
+import { AnimatePresence, motion } from 'framer-motion'
+import Link from 'next/link'
+
+export const EmployeeCalle: React.FC = () => {
+    return (
+        <div className={styles.content}>
+            <div>
+                <motion.div
+                    className={styles.images}
+                    initial={{ y: 200 }}
+                    whileInView={{ y: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                    <Image
+                        src={'/assets/employeeCalle.svg'}
+                        layout="fixed"
+                        width="250vw"
+                        height="350vw"
+                        alt={'employeeCalle'}
+                    />
+                </motion.div>
+                <motion.div
+                    className={styles.moreInfo}
+                    initial={{ y: 125 }}
+                    whileInView={{ y: 0 }}
+                    transition={{ duration: 2 }}
+                >
+                    Les mer om Calle
+                    <Link href={'#'}>her</Link>
+                </motion.div>
+            </div>
+            <motion.div
+                className={styles.textSection}
+                initial={{ y: 200 }}
+                whileInView={{ y: 0 }}
+                transition={{ duration: 1 }}
+            >
+                <p className={styles.quote}>
+                    - Noe av det beste med å være en del av dette prosjektet, er
+                    alt det nye jeg har lært, innsikten i hvordan rettsvesenet i
+                    Norge fungerer.
+                </p>
+                <p className={styles.name}>Birgitte Bright</p>
+                <p className={styles.title}>Systemutvikler, Webstep</p>
+            </motion.div>
+        </div>
+    )
+}
+
+export default EmployeeCalle
