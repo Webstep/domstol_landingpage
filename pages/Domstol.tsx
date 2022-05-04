68cdcf443eaff7783752e7c6eb8ac51a4e57bbf6
Emilie Dybendal Aulie <59264023+emilieaulie@users.noreply.github.com>
2022-05-04 13:13:37 +0200

    Update Domstol.tsx

--- pages/Domstol.tsx
+++ pages/Domstol.tsx
@@ -7,13 +7,9 @@ import AboutUs from '../components/slides/domstol/AboutUs'
 const Domstol = () => {
     return (
         <>
-<<<<<<< HEAD
             <Introduction />
-            <ExampleSlide />
-=======
-            <ConvictedPercent />
->>>>>>> 3ab9a3c30b155df2ebe225f41bfcb6ad4eb33bb7
             <AboutUs />
+            <ConvictedPercent />
             <ExampleSlide />
         </>
     )
