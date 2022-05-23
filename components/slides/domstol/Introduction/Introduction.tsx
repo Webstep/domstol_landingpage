269f21010bf21708225b8bf36ad10bf9b2e402a4
André Schjøth <aschjoth@gmail.com>
2022-05-23 15:10:36 +0200

    fix: more consistent scroll

--- components/slides/domstol/Introduction/Introduction.tsx
+++ components/slides/domstol/Introduction/Introduction.tsx
@@ -18,53 +18,55 @@ const DomstolIntroduction: React.VFC<IntroductionProps> = ({ allowScrolling }) =
     }, [allowScrolling, inView]);
 
     return (
-        <section className={styles.container}>
-            <div className={styles.overlay}>
-                <div className={styles.information}>
-                    <Image src={Weight} alt="" height="41" width="48" />
-                    <h1>
-                        DIGITALISERING <br /> AV DOMSTOLENE
-                    </h1>
-                    <p>
-                        Maktfordelingsprinsippet og de norske domstolenes
-                        uavhengighet er som stoffet din og min frihet er laget
-                        av. I flere europeiske land er Domstolenes uavhengighet
-                        truet. I Norge er uavhengigheten sterk og tilliten høy.
-                        Og den øker år for år. Det skal vi være glad for. Men
-                        tilliten til domstolene kommer med med et like stort
-                        ansvar.
-                    </p>
-                    <div className={styles.scrollDown}>
-                        <span>Scroll</span>
-                        <motion.div
-                            animate={{
-                                y: [0, 8, 0]
-                            }}
-                            transition={{
-                                repeat: Infinity,
-                                duration: 1.5
-                            }}>
+        <>
+            <section className={styles.container}>
+                <div className={styles.overlay}>
+                    <div className={styles.information}>
+                        <Image src={Weight} alt="" height="41" width="48" />
+                        <h1>
+                            DIGITALISERING <br /> AV DOMSTOLENE
+                        </h1>
+                        <p>
+                            Maktfordelingsprinsippet og de norske domstolenes
+                            uavhengighet er som stoffet din og min frihet er laget
+                            av. I flere europeiske land er Domstolenes uavhengighet
+                            truet. I Norge er uavhengigheten sterk og tilliten høy.
+                            Og den øker år for år. Det skal vi være glad for. Men
+                            tilliten til domstolene kommer med med et like stort
+                            ansvar.
+                        </p>
+                        <div className={styles.scrollDown}>
+                            <span>Scroll</span>
+                            <motion.div
+                                animate={{
+                                    y: [0, 8, 0]
+                                }}
+                                transition={{
+                                    repeat: Infinity,
+                                    duration: 1.5
+                                }}>
 
-                            <ArrowButton
-                                direction={Direction.Down}
-                                onClick={() => { }}
-                                alt='pil ned'
-                            />
-                        </motion.div>
+                                <ArrowButton
+                                    direction={Direction.Down}
+                                    onClick={() => { }}
+                                    alt='pil ned'
+                                />
+                            </motion.div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className={styles.tintedBackground} />
-            <div className={styles.videoContainer}>
-                <video
-                    src={require('../../../../public/assets/domstol/videos/Webstep_DA_illustrasjon_v02.mp4')}
-                    autoPlay
-                    muted
-                    loop
-                ></video>
-            </div>
+                <div className={styles.tintedBackground} />
+                <div className={styles.videoContainer}>
+                    <video
+                        src={require('../../../../public/assets/domstol/videos/Webstep_DA_illustrasjon_v02.mp4')}
+                        autoPlay
+                        muted
+                        loop
+                    ></video>
+                </div>
+            </section>
             <div className={styles.breakpoint} ref={ref}></div>
-        </section>
+        </>
     )
 }
 
