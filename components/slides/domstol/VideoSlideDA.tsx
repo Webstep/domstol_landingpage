dbd2baeee185ce04339df03410c36e87daedd508
karenhompland <khompland@gmail.com>
2022-06-08 22:07:24 +0200

    cleaned up old videoslides, removed citation/text

--- components/slides/domstol/VideoSlideDA.tsx
+++ components/slides/domstol/VideoSlideDA.tsx
@@ -6,11 +6,7 @@ import styles from './VideoSlide/VideoSlide.module.scss';
 const VideoSlideDA = () => {
     return (
         <>
-        <VideoSlide src={require('../../../public/videos/Webstep_DA_intervju_DA_1208x1920_v03.mp4')} autoplay={true} center={0.2}>
-            <div className={styles.citation}>
-                    <Citation name={"Herman Seip"} quote={"Vi skal være best i verden!"} quoteType={"short"} position={"Arkitekt hos DA"}/>
-            </div> 
-        </VideoSlide>
+        <VideoSlide src={require('../../../public/videos/Webstep_DA_intervju_DA_1208x1920_v03.mp4')} autoplay={true}/>
         </>
     );
 };
--- components/slides/domstol/VideoSlideWebstep.tsx
+++ components/slides/domstol/VideoSlideWebstep.tsx
@@ -6,11 +6,7 @@ import styles from './VideoSlide/VideoSlide.module.scss';
 const VideoSlideWebstep = () => {
     return (
         <>
-            <VideoSlide src={require('../../../public/videos/Webstep_DA_intervju_Webstep_1208x1920_v03.mp4')} autoplay={true} center={0.25} >
-            <div className={styles.citation}>
-                    <Citation name={"Carl Bøkestad "} quote={"Vi har fått lov til å være med å utvikle den teknologien som kommer til å ligge foran oss i mange år"} quoteType={"long"} position={"Systemutvikler, Webstep"}/>
-            </div> 
-        </VideoSlide>
+            <VideoSlide src={require('../../../public/videos/Webstep_DA_intervju_Webstep_1208x1920_v03.mp4')} autoplay={true} />
         </>
     );
 };
