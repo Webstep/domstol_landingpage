e7a2edfc2c315aa3329643cb9dea626dc07e144e
karenhompland <khompland@gmail.com>
2022-06-08 22:04:00 +0200

    #58 fullscreen video slide

--- components/slides/domstol/VideoSlide/VideoSlide.tsx
+++ components/slides/domstol/VideoSlide/VideoSlide.tsx
@@ -5,43 +5,22 @@ import styles from './VideoSlide.module.scss';
 
 interface VideoSlideProps {
     src: string;
-    center: number;
     autoplay: boolean
 }
 
 const VideoSlide: React.FC<VideoSlideProps> = (props) => {
-    const [height, setHeight] = useState<number>(500);
-    const [textHeight, setTextHeight] = useState<number>(50);
-
-    useEffect(() => {
-        setHeight(window.innerHeight - 100)
-        setTextHeight((window.innerHeight / 2) - window.innerHeight * props.center);
-    }, [props.center])
-
 
     return (
         <>
             <section>
-                <div className={styles.container}>
-                    <div className={styles.video}>
-                        <motion.div
-                            initial={{ y: height }}
-                            whileInView={{ y: 0 }}
-                            transition={{ duration: 0.5 }}
-                            viewport={{ once: false }}
-                        >
-                            <Video src={props.src} autoplay={props.autoplay}></Video>
-                        </motion.div>
-                    </div>
-                    <motion.div
-                        initial={{ y: height }}
-                        whileInView={{ y: textHeight }}
-                        transition={{ duration: 0.5 }}
-                        viewport={{ once: false }}
-                    >
-                        {props.children}
-                    </motion.div>
-                </div>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                    <Video src={props.src} autoplay={props.autoplay}></Video>
+                </motion.div>
             </section>
         </>
     );
