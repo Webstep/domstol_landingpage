caf3dcf05594dab2c72379afcd3170c1557eae4f
Emilie Dybendal Aulie <59264023+emilieaulie@users.noreply.github.com>
2022-08-01 20:58:38 +0200

    feat: draft for mobile version

--- components/slides/domstol/ImportantCompetency/ImportantCompetency.tsx
+++ components/slides/domstol/ImportantCompetency/ImportantCompetency.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './ImportantCompetency.module.scss';
 import Image from 'next/image';
 import SamfunnsAnsvar from '../../../../public/assets/important-competency.jpg';
+import Text from '../../../common/Text'
 
 const ImportantCompetency: React.FC = () => {
 
@@ -18,23 +19,29 @@ const ImportantCompetency: React.FC = () => {
     return (
         <section className={styles.section}>
             <div className={styles.container}>
-                <motion.div
-                    className={styles.image}
-                    initial={{ y: -200, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1, transition: enterTransistion }}
-                    exit={{ x: -100, opacity: 0, transition: exitTransition }}
-                >
-                    <Image src={SamfunnsAnsvar} alt="" width="358px" height="467px" />
-                </motion.div>
-                <div className={styles.text}>
+                <div className={styles.media}>
+                    <motion.div
+                        className={styles.image}
+                        initial={{ y: -200, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1, transition: enterTransistion }}
+                        exit={{ x: -100, opacity: 0, transition: exitTransition }}
+                    >
+                        <Image src={SamfunnsAnsvar} alt="" width="358px" height="467px" />
+                    </motion.div>
+                </div>
+                <div className={styles.textSection}>
                     <motion.div
                         initial={{ y: -200, opacity: 0 }}
                         animate={{ y: 0, opacity: 1, transition: enterTransistion }}
                         exit={{ x: 100, opacity: 0, transition: exitTransition }}
                     >
                         <h1 className={styles.heading}>
-                            TRENGER VIKTIG KOMPETANSE FRA WEBSTEP
+                            <div className={styles.stickyDiv}>
+                                TRENGER VIKTIG KOMPETANSE FRA WEBSTEP
+                            </div>
                         </h1>
+
+
                     </motion.div>
 
                     <motion.div
@@ -43,23 +50,26 @@ const ImportantCompetency: React.FC = () => {
                         exit={{ y: 100, opacity: 0, transition: exitTransition }}
                     >
                         <div className={styles.description}>
-                            <p>Dagens DA-løsning er en hybridløsning, dels systemstøtte, dels manuelle operasjoner. Ting tar tid fra beslutning til resultat. Men hvordan skulle de løse de teknologiske problemene? Herman har svaret:
-                                <br />
-                                <br />
+                            <Text isOverlay>
+                                Dagens DA-løsning er en hybridløsning, dels systemstøtte, dels manuelle operasjoner.</Text>  <Text isOverlay>Ting tar tid fra beslutning til resultat. Men hvordan skulle de løse de teknologiske problemene? Herman har svaret:</Text>
+                            <br />
+                            <br />
+                            <blockquote>
                                 -  Vi trengte bred digital kompetanse, og vi hadde behov for de beste. Webstep har vist oss at de både har kompetansen og kapasiteten til å hjelpe oss på denne veien.
-                            </p>
-                            <p>
-                                Dette er en stor og spennende oppgave, og Webstep bygger sterke software-team for å nå hårete DA-mål. Som nå er blitt våre.
-                                <br />
-                                <br />
+                            </blockquote>
+                            <Text isOverlay>
+                                Dette er en stor og spennende oppgave, og Webstep bygger sterke software-team for å nå hårete DA-mål. Som nå er blitt våre.</Text>
+                            <br />
+                            <br />
+                            <blockquote>
                                 -  Alle som kommer inn døra her ønsker å levere på samfunns- oppdraget vårt.
-                            </p>
+                            </blockquote>
                         </div>
                     </motion.div>
                 </div>
 
-            </div>
-        </section>
+            </div >
+        </section >
     );
 };
 
