e1a2b9d32b8593a3ff253a5ea5aebdf1bf224221
André Schjøth <aschjoth@gmail.com>
2022-09-11 13:57:51 +0200

    Pwa (#105)
    * updated metadata and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,16 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Head>
                 <title>Webstep</title>
                 <meta name="Webstep historier" content="Lorem ipsum" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/meta/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/meta/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/meta/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/meta/favicon-16x16.png" />
+                <link rel="manifest" href="/meta/site.webmanifest" />
+                <link rel="mask-icon" href="/meta/safari-pinned-tab.svg" color="#1c1e4e" />
+                <link rel="shortcut icon" href="/meta/favicon.ico" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="msapplication-config" content="/meta/browserconfig.xml" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <article className={scrollStyles.case}>
                 <Navbar />
