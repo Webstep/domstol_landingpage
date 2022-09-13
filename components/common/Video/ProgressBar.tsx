521f117bad89afc322efae3c2540c228ebf5286b
André Schjøth <aschjoth@gmail.com>
2022-09-13 11:42:27 +0200

    fix: setting correct duration & refactoring

--- components/common/Video/ProgressBar.tsx
+++ components/common/Video/ProgressBar.tsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import styles from './Video.module.scss'
 
 interface ProgressBarProps {
-    progress: number
+    duration: number | undefined,
+    progress: number,
+    onClick: (newProgress: number) => void;
 }
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
-    const progressBarWidth = progress + '%'
+const ProgressBar: React.FC<ProgressBarProps> = ({ duration, progress, onClick }) => {
+    const [buttonWidth, setButtonWidth] = useState<number>(0)
+    const buttonRef = useRef<HTMLButtonElement>(null)
+
+    const progressClick = (e: { nativeEvent: { offsetX: number } }) => {
+        const click = (e.nativeEvent.offsetX / buttonWidth) * 100
+        if (!duration) return;
+
+        const newProgress = (duration / 100) * click
+        onClick(newProgress)
+    }
+
+    useEffect(() => {
+        setButtonWidth(buttonRef?.current?.clientWidth || 100);
+    }, []);
+
+    const progressBarWidth = duration ? (progress / duration * 100) : 0 + '%'
+
+    const formattedDuration = useMemo(() => formatTime(duration), [duration])
 
     return (
-        <>
-            <div className={styles.progressBarContainer}>
-                <div
-                    className={styles.progressBar}
-                    style={{ width: progressBarWidth }}
-                />
-            </div>
-        </>
+        <div className={styles.timeControl}>
+            <span>{formatTime(progress)}</span>
+
+            <button ref={buttonRef} onClick={(e) => progressClick(e)}>
+                <div className={styles.progressBarContainer}>
+                    <div
+                        className={styles.progressBar}
+                        style={{ width: progressBarWidth }}
+                    />
+                </div>
+            </button>
+            <span>{formattedDuration}</span>
+        </div>
     )
 }
 
 export default ProgressBar
+
+const formatTime = (time: number | undefined): string => time ?
+    Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2) : "00:00"
--- components/common/Video/Video.tsx
+++ components/common/Video/Video.tsx
@@ -13,17 +13,13 @@ interface VideoProps {
 }
 
 const Video: React.FC<VideoProps> = (props) => {
-    const buttonRef = useRef<HTMLButtonElement>(null)
     const videoRef = useRef<HTMLVideoElement>(null)
     const [isPlaying, setIsPlaying] = useState<boolean>(props.autoplay)
     const [isSound, setIsSound] = useState<boolean>(!props.autoplay)
-    const [duration, setDuration] = useState<number>(0)
+    const [duration, setDuration] = useState<number>()
     const [currentTime, setCurrentTime] = useState<number>(0)
-    const [progress, setProgress] = useState<number>(0)
-    const [buttonWidth, setButtonWidth] = useState<number>(0)
 
     const play = () => {
-        setDuration(videoRef.current?.duration || 1);
         videoRef?.current?.play()
         setIsPlaying(true)
     }
@@ -41,13 +37,9 @@ const Video: React.FC<VideoProps> = (props) => {
     }
 
     useEffect(() => {
-        setButtonWidth(buttonRef?.current?.clientWidth || 100);
-        setDuration(videoRef.current?.duration || 71);
-
         const incrementTime = () => {
             const time = videoRef.current?.currentTime ?? 0
             setCurrentTime(time)
-            setProgress((time / duration) * 100)
         }
 
         const intervall = setInterval(incrementTime, 1000)
@@ -55,20 +47,25 @@ const Video: React.FC<VideoProps> = (props) => {
         return () => {
             clearInterval(intervall)
         }
-    }, [buttonWidth, duration])
+    }, [duration])
 
-
-    const formattedDuration = useMemo(() => formatTime(duration), [duration])
-
-    const progressClick = (e: { nativeEvent: { offsetX: number } }) => {
-        const click = (e.nativeEvent.offsetX / buttonWidth) * 100
-        const progressTime = (duration / 100) * click
-        if (videoRef.current !== null) { setProgress(progressTime); videoRef.current.currentTime = progressTime; videoRef.current.play; }
+    const jumpToTime = (newTime: number) => {
+        if (videoRef.current !== null) {
+            videoRef.current.currentTime = newTime;
+            videoRef.current.play;
+            setCurrentTime(newTime)
+        }
     }
 
     return (
         <div className={styles.container}>
-            <video onClick={() => { isPlaying ? pause() : play(); }} src={props.src} ref={videoRef} autoPlay={props.autoplay} disablePictureInPicture muted={props.autoplay} onLoad={e => console.log(e)} />
+            <video
+                onClick={() => { isPlaying ? pause() : play(); }}
+                src={props.src} ref={videoRef} autoPlay={props.autoplay} disablePictureInPicture
+                muted={props.autoplay}
+                onLoadedMetadata={() => setDuration(videoRef.current?.duration)}
+
+            />
             <div className={styles.controls}>
                 <div className={styles.buttonGroup}>
                     <button
@@ -90,11 +87,7 @@ const Video: React.FC<VideoProps> = (props) => {
                         />
                     </button>
                 </div>
-                <div className={styles.timeControl}>
-                    <span>{formatTime(currentTime)}</span>
-                    <button ref={buttonRef} onClick={(e) => progressClick(e)}><ProgressBar progress={progress} /></button>
-                    <span>{formattedDuration}</span>
-                </div>
+                <ProgressBar duration={duration} progress={currentTime} onClick={jumpToTime} />
             </div>
         </div>
     )
@@ -102,5 +95,4 @@ const Video: React.FC<VideoProps> = (props) => {
 
 export default Video
 
-const formatTime = (time: number) => Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
 
