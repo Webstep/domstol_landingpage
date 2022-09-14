9a7e38d5cfab061a1daeb0493d1c422fd8f02b23
André Schjøth <aschjoth@gmail.com>
2022-09-14 12:38:17 +0200

    add animation to collaboration slide

--- components/slides/domstol/Collaboration/Collaboration.tsx
+++ components/slides/domstol/Collaboration/Collaboration.tsx
@@ -4,23 +4,25 @@ import Image from 'next/image'
 import BrigittAndHerman from '../../../../public/assets/domstol/images/BrigittAndHerman_horizontal.jpg';
 import Text from '../../../common/Text';
 import Citation from '../../../common/Citation';
+import { motion, Variants } from 'framer-motion';
 
 const Collaboration: React.VFC = () => {
     return (
         <section className={styles.section}>
             <div className={styles.imageContainer}>
-                <Image
-                    src={BrigittAndHerman}
-                    width={463}
-                    height={309}
-                    alt="Brigitt og Herman i dialog"
-                />
-                <video width={574} height={327} loop muted autoPlay>
+                <motion.div {...imageVariants}>
+                    <Image
+                        src={BrigittAndHerman}
+                        width={463}
+                        height={309}
+                        alt="Brigitt og Herman i dialog"
+                    />
+                </motion.div>
+                <motion.video width={574} height={327} loop muted autoPlay {...videoVariants}>
                     <source src="/assets/domstol/videos/Courtroom_loop_16x9.mp4" type="video/mp4" />
-                </video>
-
+                </motion.video>
             </div>
-            <div className={styles.description}>
+            <motion.div className={styles.description} {...containerVariants}>
                 <div>
                     <h2>webstep og domstolene</h2>
                     <Text>
@@ -33,10 +35,73 @@ const Collaboration: React.VFC = () => {
                 <Text isOverlay>
                     Domstolene er en massiv konfliktløser i det norske samfunnet. En strøm av skyld og uskyld, rett og galt strømmer gjennom.  Det gir mening å bygge så viktige systemer for en stolt og offensiv oppdragsgiver som domstoladministrasjonen. Da blir jobben som å bygge en katedral, i motsetning til å hugge stein.
                 </Text>
-            </div>
+            </motion.div>
 
         </section>
     );
 };
 
-export default Collaboration;
+
+
+const containerVariants: Variants = {
+    initial: { x: 100, y: 50, opacity: 0 },
+    animate: {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+        }
+    },
+    exit: {
+        x: 100,
+        y: 50,
+        opacity: 0,
+        transition: {
+            duration: 1,
+        }
+    }
+}
+
+const videoVariants: Variants = {
+    initial: {
+        y: 100,
+        x: -50,
+        opacity: 0,
+    },
+    animate: {
+        y: 0,
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1
+        }
+    },
+    exit: {
+        y: 100, x: -50, opacity: 0,
+        transition: { duration: 1 }
+    }
+}
+
+const imageVariants: Variants = {
+    initial: {
+        y: -100,
+        x: 50,
+        opacity: 0,
+        zIndex: 999
+    },
+    animate: {
+        y: 0,
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1
+        }
+    },
+    exit: {
+        y: -100, x: 50, opacity: 0,
+        transition: { duration: 1 }
+    }
+}
+
+export default Collaboration;   
