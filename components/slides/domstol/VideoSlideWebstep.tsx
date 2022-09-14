22db30fdb47226fd18840b394e384f4cbcd6588f
André Schjøth <aschjoth@gmail.com>
2022-09-14 11:33:51 +0200

    cropped video

--- components/slides/domstol/VideoSlideWebstep.tsx
+++ components/slides/domstol/VideoSlideWebstep.tsx
@@ -3,7 +3,8 @@ import VideoSlide from './VideoSlide';
 
 const VideoSlideWebstep = () => {
     return (
-        <VideoSlide src={require('../../../public/assets/domstol/videos/Webstep_DA_intervju_Webstep_1208x1920_v03.mp4')} autoplay={true} />
+        // <VideoSlide src={require('../../../public/assets/domstol/videos/Webstep_DA_intervju_Webstep_1208x1920_v03.mp4')} autoplay={true} />
+        <VideoSlide src={require('../../../public/assets/domstol/videos/webstep_cropped.mp4')} autoplay={true} />
     );
 };
 
