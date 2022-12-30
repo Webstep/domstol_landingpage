624b6aec206691991f76f29034095077b0f1011d
Seran Shanmugathas <seran@live.no>
2022-12-30 13:31:11 +0100

    feat: add CTO citation slide (#129)

--- pages/Arealize.tsx
+++ pages/Arealize.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ClearGoals from "../components/slides/arealize/ClearGoals";
+import CtoCitation from "../components/slides/arealize/CtoCitation";
 import Introduction from "../components/slides/arealize/Introduction";
 
 const Arealize = () => {
     return (
         <>
-            <Introduction allowScrolling={() => {/* remove this parameter if we decide to not use slides*/ }} />
+            <Introduction allowScrolling={() => {
+                // remove this parameter if we decide to not use slides 
+            }} />
             <ClearGoals />
+            <CtoCitation />
         </>
     );
 };
