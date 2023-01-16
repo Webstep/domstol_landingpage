ce6699c1a1ceaf8e1cb47d511b229659b6a325df
André Schjøth <aschjoth@gmail.com>
2023-01-16 16:49:32 +0100

    chore: cleanup

--- components/slides/arealize/ClearGoals/ClearGoals.tsx
+++ components/slides/arealize/ClearGoals/ClearGoals.tsx
@@ -40,74 +40,72 @@ const ClearGoals: React.FC = () => {
     }
 
     return (
-        <>
-            <section className={styles.section}>
-                <div className={styles.container}>
-                    <div className={styles.textSection}>
-                        <motion.div
-                            initial={isMobile ? {} : { y: -200, opacity: 0 }}
-                            animate={{ y: 0, opacity: 1, transition: enterTransistion }}
-                            exit={{ x: 100, opacity: 0, transition: exitTransition }}
-                        >
-                            <Title className={styles.heading}>
-                                <div className={styles.stickyDiv}>
-                                    TYDELIGE MÅL
-                                </div>
-                            </Title>
-                        </motion.div>
-
-                        <motion.div
-                            initial={isMobile ? {} : { x: 200, opacity: 0 }}
-                            animate={{ x: 0, opacity: 1, transition: enterTransistion }}
-                            exit={{ y: 100, opacity: 0, transition: exitTransition }}
-                        >
-                            <div className={styles.description}>
-                                {isMobile ?
-                                    <Text><b>Målet til Arealize er klart og tydelig:</b> <br /> De ønsker å optimalisere og effektivisere planleggingingsprosessen på en måte som er både juridisk og psykososialt ansvarlig for at arbeidsmiljøet skal bli så bra som mulig.</Text>
-                                    :
-                                    <Text>Målet er klart og tydelig: Arealize vil optimalisere og effektivisere planleggingingsprosessen på en måte som er både juridisk og psykososialt ansvarlig for at arbeidsmiljøet skal bli så bra som mulig.</Text>
-                                }
-                                <div className={styles.quote}>
-                                    <Citation
-                                        quote="Slik skaper vi arbeidsmiljø som man kan trives i, samtidig som det ser fint ut."
-                                        quoteType="short"
-                                        variants={item}
-                                    />
-                                </div>
+        <section className={styles.section}>
+            <div className={styles.container}>
+                <div className={styles.textSection}>
+                    <motion.div
+                        initial={isMobile ? {} : { y: -200, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1, transition: enterTransistion }}
+                        exit={{ x: 100, opacity: 0, transition: exitTransition }}
+                    >
+                        <Title className={styles.heading}>
+                            <div className={styles.stickyDiv}>
+                                TYDELIGE MÅL
                             </div>
-                        </motion.div>
-                    </div>
-                    <motion.div
-                        className={styles.mediaLeft}
-                        initial={{ y: 0, opacity: 0 }}
-                        animate={{ y: 0, opacity: 1, transition: enterTransistion }}
-                        exit={{ x: 0, opacity: 0, transition: exitTransition }}
-                    >
-                        <Image
-                            src={'/assets/arealize/images/clear-goal-left.png'}
-                            alt=""
-                            width={481}
-                            height={598}
-                            objectFit={'cover'}
-                        />
+                        </Title>
                     </motion.div>
+
                     <motion.div
-                        className={styles.mediaRight}
-                        initial={{ x: 0, opacity: 1 }}
-                        animate={{ y: 0, opacity: 1, transition: enterTransistion }}
-                        exit={{ x: 0, opacity: 1, transition: exitTransition }}
+                        initial={isMobile ? {} : { x: 200, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1, transition: enterTransistion }}
+                        exit={{ y: 100, opacity: 0, transition: exitTransition }}
                     >
-                        <Image
-                            src={'/assets/arealize/images/clear-goal-right.png'}
-                            alt=""
-                            width={481}
-                            height={300}
-                            objectFit={'cover'}
-                        />
+                        <div className={styles.description}>
+                            {isMobile ?
+                                <Text><b>Målet til Arealize er klart og tydelig:</b> <br /> De ønsker å optimalisere og effektivisere planleggingingsprosessen på en måte som er både juridisk og psykososialt ansvarlig for at arbeidsmiljøet skal bli så bra som mulig.</Text>
+                                :
+                                <Text>Målet er klart og tydelig: Arealize vil optimalisere og effektivisere planleggingingsprosessen på en måte som er både juridisk og psykososialt ansvarlig for at arbeidsmiljøet skal bli så bra som mulig.</Text>
+                            }
+                            <div className={styles.quote}>
+                                <Citation
+                                    quote="Slik skaper vi arbeidsmiljø som man kan trives i, samtidig som det ser fint ut."
+                                    quoteType="short"
+                                    variants={item}
+                                />
+                            </div>
+                        </div>
                     </motion.div>
                 </div>
-            </section >
-        </>
+                <motion.div
+                    className={styles.mediaLeft}
+                    initial={{ y: 0, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1, transition: enterTransistion }}
+                    exit={{ x: 0, opacity: 0, transition: exitTransition }}
+                >
+                    <Image
+                        src={'/assets/arealize/images/clear-goal-left.png'}
+                        alt=""
+                        width={481}
+                        height={598}
+                        objectFit={'cover'}
+                    />
+                </motion.div>
+                <motion.div
+                    className={styles.mediaRight}
+                    initial={{ x: 0, opacity: 1 }}
+                    animate={{ y: 0, opacity: 1, transition: enterTransistion }}
+                    exit={{ x: 0, opacity: 1, transition: exitTransition }}
+                >
+                    <Image
+                        src={'/assets/arealize/images/clear-goal-right.png'}
+                        alt=""
+                        width={481}
+                        height={300}
+                        objectFit={'cover'}
+                    />
+                </motion.div>
+            </div>
+        </section >
     );
 };
 
