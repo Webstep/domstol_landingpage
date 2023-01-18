0e06d1a142555d012c4881df79c43e9841613e89
André Schjøth <aschjoth@gmail.com>
2023-01-18 14:46:03 +0100

    feat(#132): implement desktop and mobile

--- pages/Arealize.tsx
+++ pages/Arealize.tsx
@@ -3,6 +3,7 @@ import ClearGoals from "../components/slides/arealize/ClearGoals";
 import CtoCitation from "../components/slides/arealize/CtoCitation";
 import Introduction from "../components/slides/arealize/Introduction";
 import SeniorHelp from "../components/slides/arealize/SeniorHelp";
+import WayForward from '../components/slides/arealize/WayForward';
 
 const Arealize = () => {
     return (
@@ -13,6 +14,7 @@ const Arealize = () => {
             <ClearGoals />
             <CtoCitation />
             <SeniorHelp />
+            <WayForward />
         </>
     );
 };
