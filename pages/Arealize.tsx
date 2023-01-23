aeed38e3bfb370bb07b62600978160def1669c9e
Seran Shanmugathas <seran@live.no>
2023-01-23 22:32:15 +0100

    feat: add read more slide (#133)

--- pages/Arealize.tsx
+++ pages/Arealize.tsx
@@ -3,6 +3,7 @@ import ClearGoals from "../components/slides/arealize/ClearGoals";
 import CtoCitation from "../components/slides/arealize/CtoCitation";
 import EntrepreneurialJourney from '../components/slides/arealize/EntrepreneurialJourney';
 import Introduction from "../components/slides/arealize/Introduction";
+import ReadMore from "../components/slides/arealize/ReadMore";
 import SeniorHelp from "../components/slides/arealize/SeniorHelp";
 import WayForward from '../components/slides/arealize/WayForward';
 
@@ -17,6 +18,7 @@ const Arealize = () => {
             <SeniorHelp />
             <EntrepreneurialJourney />
             <WayForward />
+            <ReadMore />
         </>
     );
 };
