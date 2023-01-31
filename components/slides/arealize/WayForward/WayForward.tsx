6742894019c5bfbdad7b30a0acc7c19b6fb5aa1e
Seran Shanmugathas <seran@live.no>
2023-01-31 22:03:07 +0100

    fix: remove image when on mobile (#151)

--- components/slides/arealize/WayForward/WayForward.tsx
+++ components/slides/arealize/WayForward/WayForward.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Citation from '../../../common/Citation';
 import { Title, Text } from '../../../common/Typography';
 import styles from './WayForward.module.scss';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import { DeviceContext } from '../../../common/Device';
 
 const WayForward = () => {
+    const { isMobile } = useContext(DeviceContext)
+
     return (
         <section className={styles.section}>
             <div className={styles.container}>
                 <motion.div
                     className={styles.video}
-                    initial={{ x: -200, opacity: 0 }}
+                    initial={isMobile ? {} : { x: -200, opacity: 0 }}
                     whileInView={{ x: 0, opacity: 1, transition: { duration: 1 } }}
                     exit={{ x: -100, opacity: 0, transition: { duration: 1 } }}
                     viewport={{ once: true }}
@@ -25,7 +28,7 @@ const WayForward = () => {
                 </motion.div>
                 <motion.div
                     className={styles.description}
-                    initial={{ x: 200, opacity: 0 }}
+                    initial={isMobile ? {} : { x: 200, opacity: 0 }}
                     whileInView={{ x: 0, opacity: 1, transition: { duration: 1 } }}
                     exit={{ x: 100, opacity: 0, transition: { duration: 1 } }}
                     viewport={{ once: true }}
@@ -34,7 +37,7 @@ const WayForward = () => {
                     <Text isOverlay>Fremtiden til Arealize ser lys ut. De er i full sving med brukertesting og vet hva neste steg i prosessen er.</Text>
                     <Citation quote="Akkurat nå har vi mye på gang. Vi har jobbet hardt og vi liker retningen vi går i. Så her er det bare å holde farten og fortsette med utviklingen!" name="Yanislav Zagorov" occupation='CTO i Arealize' />
                 </motion.div>
-                <motion.div
+                {!isMobile && <motion.div
                     className={styles.image}
                     initial={{ x: 200, opacity: 0 }}
                     whileInView={{ x: 0, opacity: 1, transition: { duration: 1 } }}
@@ -44,9 +47,9 @@ const WayForward = () => {
                     <Image src="/assets/arealize/images/way-forward.png" alt="Det jobbes hardt på Arealize kontoret"
                         width={462} height={289}
                     />
-                </motion.div>
+                </motion.div>}
             </div>
-        </section>
+        </section >
     );
 };
 
