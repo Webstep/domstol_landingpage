f682109710f744cb430962780004b6296d6f395a
Seran Shanmugathas <seran@live.no>
2023-03-06 17:44:31 +0100

    chore: enable autplay on video

--- components/slides/domstol/VideoSlideTech.tsx
+++ components/slides/domstol/VideoSlideTech.tsx
@@ -3,7 +3,7 @@ import VideoSlide from '../../common/VideoSlide';
 
 const VideoSlideTech = () => {
     return (
-        <VideoSlide src="https://s3.tebi.io/webstep/Webstep_DA_tekniskfilm_v01.mp4" autoplay={false} />
+        <VideoSlide src="https://s3.tebi.io/webstep/Webstep_DA_tekniskfilm_v01.mp4" autoplay={true} />
     );
 };
 
