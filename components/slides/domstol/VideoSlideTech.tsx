c7502212527660c10ea63dd421f71a2bff849171
Seran Shanmugathas <seran@live.no>
2023-04-06 21:59:38 +0200

    fix: use videos hosted on aws

--- components/slides/domstol/VideoSlideTech.tsx
+++ components/slides/domstol/VideoSlideTech.tsx
@@ -3,7 +3,7 @@ import VideoSlide from '../../common/VideoSlide';
 
 const VideoSlideTech = () => {
     return (
-        <VideoSlide src="https://s3.tebi.io/webstep/Webstep_DA_tekniskfilm_v01.mp4" autoplay={true} />
+        <VideoSlide src="https://d1rt9v3qpcydso.cloudfront.net/Webstep_DA_tekniskfilm_v01.mp4" autoplay={true} />
     );
 };
 
